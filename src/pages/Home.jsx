7d82bae0c275a16e5ecb36de83e0a1666c55bc58
harcharans1 <162577124+harcharans1@users.noreply.github.com>
2026-03-06 15:42:42 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+function Home(){
+
+return(
+
+<div className="container">
+
+<h1>Ik Onkar</h1>
+
+<h2>Today's Vichar</h2>
+
+<p>
+ਨਾਨਕ ਨਾਮ ਚੜ੍ਹਦੀ ਕਲਾ, ਤੇਰੇ ਭਾਣੇ ਸਰਬੱਤ ਦਾ ਭਲਾ
+</p>
+
+<audio controls>
+
+<source src="/kirtan/kirtan1.mp3" type="audio/mpeg"/>
+
+</audio>
+
+</div>
+
+)
+
+}
+
+export default Home
